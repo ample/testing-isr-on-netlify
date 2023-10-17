520d9b9d7b3c818eb3175e07c6631f7d1407d67f
Taylor MacDonald <tcmacdonald@gmail.com>
2023-10-17 14:36:58 -0400

    Make CMS query during staticprops

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,28 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import axios from "axios";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home({ ts }) {
+export default function Home({ title, ts }) {
   return (
     <>
-      <main className={`${styles.main} ${inter.className}`}>{ts}</main>
+      <main className={`${styles.main} ${inter.className}`}>
+        {title} - {ts}
+      </main>
     </>
   );
 }
 
 export const getStaticProps = async () => {
+  const res = await axios.get(
+    `https://cdn.contentful.com/spaces/lkrmxse64d8p/environments/master/entries/QqTCYM8ByheGp68DLkOCM?access_token=${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`
+  );
+  const { data: { fields: { title } = {} } = {} } = res;
   return {
     revalidate: 10,
     props: {
+      title,
       ts: Date.now(),
     },
   };
