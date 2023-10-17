8c958de1f79f3171822994d7ee349c3f394594ca
Taylor MacDonald <tcmacdonald@gmail.com>
2023-10-17 15:36:56 -0400

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,14 @@ import { useEffect } from "react";
 import styles from "@/styles/Home.module.css";
 import axios from "axios";
 
-// const getAPI = () => `https://main--lively-puppy-196937.netlify.app/api/test`;
-
 export default function Home({ title, ts }) {
   useEffect(() => {
-    setTimeout(async () => {
-      const res = await axios.get(getAPI());
-      console.log(JSON.stringify(res, null, 2));
-    }, 15000);
+    if (typeof window !== undefined) {
+      setTimeout(async () => {
+        const res = await axios.get(`/api/test`);
+        console.log(JSON.stringify(res, null, 2));
+      }, 15000);
+    }
   }, []);
 
   return (
@@ -23,7 +23,7 @@ export default function Home({ title, ts }) {
 
 export const getStaticProps = async () => {
   const res = await axios.get(
-    `https://cdn.contentful.com/spaces/lkrmxse64d8p/environments/master/entries/QqTCYM8ByheGp68DLkOCM?access_token=${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`
+    `https://main--lively-puppy-196937.netlify.app/api/test`
   );
   const { title } = res;
   return {
