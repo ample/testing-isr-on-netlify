eba224ae1fdae444db0eb33f4eaecaf7df8222e0
Taylor MacDonald <tcmacdonald@gmail.com>
2023-10-17 15:41:59 -0400

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,10 @@ export default function Home({ title, ts }) {
   useEffect(() => {
     if (typeof window !== undefined) {
       setTimeout(async () => {
-        const res = await axios.get(`/api/test`);
-        console.log(JSON.stringify(res, null, 2));
+        const {
+          data: { title },
+        } = await axios.get(`/api/test`);
+        console.log(JSON.stringify(title, null, 2));
       }, 15000);
     }
   }, []);
